6a7457c3349f6c7415e623c962c2b7632b29242d
nicolasmisseri <nicomisseri1@gmail.com>
2022-10-13 10:47:55 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cartRouter = require("./routes/cart");
+const productsRouter = require("./routes/products");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/products", productsRouter);
+app.use("/api/cart", cartRouter);
+
+app.listen(PORT, () => console.log(`Se muestra en el puerto ${PORT}`));
---
+++ src/utils/admin.js
@@ -0,0 +1,4 @@
+module.exports = (req, res, next) => {
+  req.admin = req.get("admin");
+  next();
+};
